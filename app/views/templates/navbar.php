9664ce9226cdd0c4700b801769199a9ae27a6698
Hermes <hermes.skilled@yahoo.com>
2016-10-21 18:58:12 +0300

    Home page + navbar

--- app/views/templates/navbar.php
+++ app/views/templates/navbar.php
@@ -1,11 +1,41 @@
-	<div class="navbar navbar-inverse navbar-fixed navbar-fixed-top" id="navbar-top" >
-		<div class="container-fluid">
-			<div class="navbar-header">
-				<h3>BuddyTrip</h3>
-			</div>
-		
-					 	
-					</div>
-					
-				
-			</div>
+
+    <!-- Navigation -->
+ <nav class="navbar navbar-findcond navbar-fixed-top">
+    <div class="container">
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar">
+				<span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+			<a class="navbar-brand" href="#"><i class="fa fa-globe" aria-hidden="true"></i> BuddyFinder</p></a>
+		</div>
+		<div class="collapse navbar-collapse" id="navbar">
+			<ul class="nav navbar-nav navbar-right">
+				<li class="dropdown">
+					<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-fw fa-bell-o"></i> Notifications <span class="badge">4</span></a>
+					<ul class="dropdown-menu" role="menu">
+						<li><a href="#"><i class="fa fa-fw fa-user-plus" aria-hidden="true"></i> <span class="badge">Paul</span> accept your invitation for<span class="badge">event</span></a></li>
+						<li><a href="#"><i class="fa fa-fw fa-thumbs-o-up"></i> <span class="badge">Dinu</span> said: <i>HiddenLOL is my life!</i></a></li>
+						<li><a href="#"><i class="fa fa-fw fa-thumbs-o-down"></i> <span class="badge">Anett</span> said: <i>Nem akarom, hogy részt!</i></a></li>
+						<li><a href="#"><i class="fa fa-fw fa-thumbs-o-up"></i> <span class="badge">Robert</span> wants to be fucked in the ass</a></li>
+					</ul>
+				</li>
+				<li class="dropdown">
+					<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Schiau Sebastian<span class="caret"></span></a>
+					<ul class="dropdown-menu" role="menu">
+						<li><a href="#"><i class="fa fa-user" aria-hidden="true"></i> My profile</a></li>
+						<li><a href="#"><i class="fa fa-calendar" aria-hidden="true"></i> My events</a></li>
+						<li class="divider"></li>
+						<li><a href="#"><i class="fa fa-wrench" aria-hidden="true"></i> Settings</a></li>
+						<li><a href="#exit"><i class="fa fa-sign-out" aria-hidden="true"></i> Sign-out</a></li>
+					</ul>
+				</li>
+			</ul>
+			<form class="navbar-form navbar-right search-form" role="search">
+				<input type="text" class="form-control" placeholder="Search" />
+			</form>
+		</div>
+	</div>
+</nav>
--- index.php
+++ index.php
@@ -1,7 +1,13 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>BuddyFinder</title>
+	<title>BuddyFinder</title>	
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
 	<?php include("app/views/templates/header.php"); ?>
 
 
@@ -10,11 +16,36 @@
 <?php 
 require 'app/views/templates/navbar.php';
 ?>
+<body>
 
-    <div class="container">
-    	<div class="featuredContainer">
-        	<center><h1 class="brand-heading">BuddyFinder</h1></center>
-    	</div>
-    </div><!-- /container -->
+    <!-- Header -->
+    <a name="about"></a>
+    <div class="intro-header">
+        <div class="container">
+
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="intro-message">
+                        <h1>Buddy Finder</h1>
+                        <h3>Rediscover your city</h3>
+                        <hr class="intro-divider">
+                        <ul class="list-inline">
+                            <li>
+                                <a href="#" class="btn btn-default btn-lg"><i class="fa fa-sign-in" aria-hidden="true"></i> Login</a>
+                            </li>
+                            <li>
+                                <a href="#" class="btn btn-default btn-lg"><i class="fa fa-user-plus" aria-hidden="true"></i> Register</a>
+                            </li>
+                            
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        <!-- /.container -->
+    </div>
+    <!-- /.intro-header -->
 </body>
+
 </html>
